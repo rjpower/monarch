402ec0f12616931199a61dce9beddf3db40fd829
suo <suo@fb.com>
2025-06-20 15:50:48 -0700

    [monarch] Make test_python_actor.py work if cuda is not available
    skips tests as necessary
    
    Differential Revision: [D77054935](https://our.internmc.facebook.com/intern/diff/D77054935/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Meta-specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D77054935/)!
    
    ghstack-source-id: 291759401
    Pull Request resolved: https://github.com/pytorch-labs/monarch/pull/314

--- python/tests/test_python_actors.py
+++ python/tests/test_python_actors.py
@@ -36,6 +36,11 @@ from monarch.mesh_controller import spawn_tensor_engine
 from monarch.proc_mesh import local_proc_mesh, proc_mesh
 from monarch.rdma import RDMABuffer
 
+needs_cuda = pytest.mark.skipif(
+    not torch.cuda.is_available(),
+    reason="CUDA not available",
+)
+
 
 class Counter(Actor):
     def __init__(self, v: int):
@@ -116,6 +121,7 @@ class ParameterClient(Actor):
         return self.buffer
 
 
+@needs_cuda
 async def test_proc_mesh_rdma():
     proc = await proc_mesh(gpus=1)
     server = await proc.spawn("server", ParameterServer)
@@ -284,6 +290,7 @@ class GeneratorActor(Actor):
         ), f"{torch.sum(self.generator.weight.data)=}, {self.step=}"
 
 
+@needs_cuda
 async def test_gpu_trainer_generator():
     trainer_proc = await proc_mesh(gpus=1)
     gen_proc = await proc_mesh(gpus=1)
@@ -313,6 +320,7 @@ async def test_sync_actor():
     assert r == 5
 
 
+@needs_cuda
 def test_gpu_trainer_generator_sync() -> None:
     trainer_proc = proc_mesh(gpus=1).get()
     gen_proc = proc_mesh(gpus=1).get()
