6a84f88332a79148ff4ae764bf77aefa207de8c4
Sam Lurye <slurye@meta.com>
2025-09-25 12:29:29 -0700

    [monarch][hyperactor] Additional dims per host when spawning proc mesh on host mesh
    Add an API to specify an `Extent` for adding additional dims on each host when spawning a proc mesh on a v1 host mesh. The extent of the proc mesh will be the extent of the host mesh + the extent per host.
    
    Differential Revision: [D83274881](https://our.internmc.facebook.com/intern/diff/D83274881/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Meta-specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D83274881/)!
    
    [ghstack-poisoned]

--- hyperactor_mesh/src/v1/actor_mesh.rs
+++ hyperactor_mesh/src/v1/actor_mesh.rs
@@ -343,6 +343,7 @@ mod tests {
     use hyperactor::context::Mailbox as _;
     use hyperactor::mailbox;
     use hyperactor::supervision::ActorSupervisionEvent;
+    use ndslice::Extent;
     use ndslice::ViewExt;
     use ndslice::extent;
     use ndslice::view::Ranked;
@@ -526,7 +527,10 @@ mod tests {
     async fn test_cast() {
         let instance = testing::instance().await;
         let host_mesh = testing::host_mesh(extent!(host = 4)).await;
-        let proc_mesh = host_mesh.spawn(instance, "test").await.unwrap();
+        let proc_mesh = host_mesh
+            .spawn(instance, "test", Extent::unity())
+            .await
+            .unwrap();
         let actor_mesh = proc_mesh
             .spawn::<testactor::TestActor>(instance, "test", &())
             .await
